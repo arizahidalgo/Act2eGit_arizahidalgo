f717edde52e5ef1ed100568c78ab9ae451bccc6b
arizahidalgo <a_javier.ariza.hidalgo@iespablopicasso.es>
2021-11-30 12:34:21 +0100

    commit

--- src/eGit/numeros.java
+++ src/eGit/numeros.java
@@ -3,5 +3,6 @@ package eGit;
 public class numeros {
 	public static void main(String[] args) {
 		System.out.println("creacion del archivo");
+		System.out.println("modificacion desde el main");
 	}
 }
